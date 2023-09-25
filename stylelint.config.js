e1116a37a8fd291e10fae192123c9660be09035f
s1f102103707 <s1f102103707@iniad.org>
2023-09-25 16:07:07 +0900

    対処２

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,23 +1,6 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {
-    'selector-class-pattern': '^[a-z][a-zA-Z0-9]+$',
-    'order/properties-order': [
-      // 指定したプロパティの順序に従ってカスタマイズ
-      'display',
-      'flex-direction',
-      'padding',
-      'background-color',
-      'margin',
-      'border',
-      'text-align',
-      'width',
-    ],
-  },
+  rules: {},
 };
