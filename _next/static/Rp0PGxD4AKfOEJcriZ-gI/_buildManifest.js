24adc95db5494f31b595cef3786dc11205189def
s1f102103707 <s1f102103707@users.noreply.github.com>
2023-09-25 07:08:34 +0000

    deploy: e1116a37a8fd291e10fae192123c9660be09035f

---
+++ _next/static/Rp0PGxD4AKfOEJcriZ-gI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/fd6ab10e8fac73f4.css","static/chunks/pages/index-32c72ca10aafe077.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
